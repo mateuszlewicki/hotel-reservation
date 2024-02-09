390337de70e0cbf5c091d068ceef61de06a32618
mateusz.lewicki <mateusz.lewicki@windowslive.com>
2024-02-09 01:20:26 +0100

    fix: SQL syntax

--- backend/src/database/sqlite/init_data.sql
+++ backend/src/database/sqlite/init_data.sql
@@ -1,22 +1,22 @@
 -- initial room types 
-INSERT INTO room_types(type) VALUES ('Queen'); -- 1
-INSERT INTO room_types(type) VALUES ('Twin'); -- 2
-INSERT INTO room_types(type) VALUES ('Family'); -- 3
-INSERT INTO room_types(type) VALUES ('King'); -- 4
-INSERT INTO room_types(type) VALUES ('Executive'); -- 5
-INSERT INTO room_types(type) VALUES ('Presidential'); -- 6
+INSERT INTO room_types (type) VALUES ('Queen'); -- 1
+INSERT INTO room_types (type) VALUES ('Twin'); -- 2
+INSERT INTO room_types (type) VALUES ('Family'); -- 3
+INSERT INTO room_types (type) VALUES ('King'); -- 4
+INSERT INTO room_types (type) VALUES ('Executive'); -- 5
+INSERT INTO room_types (type) VALUES ('Presidential'); -- 6
 -- initial user (admin - 1, chatbot - 2, dummyclient - 3 ) 
 -- ONLY USED IN DEV 
 -- TODO: CHANGE LATER
-INSERT INTO users (username, password, fname, lname, email)
+INSERT INTO users (username, password, first_name, last_name, email)
 VALUES 
-    ('admin','21232f297a57a5a743894a0e4a801fc3','Super','User','lordvader@localhost');
-    ('chatbot','00f2b1ead3a0990b818517356cb40280','Chat','Bot','terminator@localhost');
+    ('admin','21232f297a57a5a743894a0e4a801fc3','Super','User','lordvader@localhost'),
+    ('chatbot','00f2b1ead3a0990b818517356cb40280','Chat','Bot','terminator@localhost'),
     ('dummyclient','49a5fa65367aeb1100747a42fbc96109','Dummy','Client','dummyclient@localhost');
 -- initial roles
 INSERT INTO roles (role_hash, role_name) 
 VALUES
-    ('CLI', 'Client') -- 1
+    ('CLI', 'Client'), -- 1
     ('SUP', 'Super Admin'), -- 2
     ('GVI', 'Global View'), -- 3
     ('GAD', 'Glboal Add'), -- 4
@@ -25,7 +25,7 @@ VALUES
     ('BVI', 'Booking View'), -- 7
     ('BAD', 'Booking Add'), -- 8
     ('BUP', 'Booking Update'), -- 9
-    ('BDE', 'Booking Delete'), -- 10
+    ('BDE', 'Booking Delete'); -- 10
 
 -- initial role binding
 INSERT INTO user_roles (user_id,role_id)
--- backend/src/database/sqlite/schema.sql
+++ backend/src/database/sqlite/schema.sql
@@ -1,5 +1,5 @@
 -- Flush database
-PRAGMA foreign_keys = OFF
+PRAGMA foreign_keys = OFF;
 
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS users;
@@ -9,45 +9,44 @@ DROP TABLE IF EXISTS extras;
 DROP TABLE IF EXISTS bookings;
 DROP TABLE IF EXISTS room_types;
 DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS baskets;
 
-PRAGMA foreign_keys = ON
+PRAGMA foreign_keys = ON;
 
 -- Create tables
 
 -- roles for RBAC
 
-CREATE TABLE roles {
+CREATE TABLE roles (
     role_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    role_name TEXT UNIQUE NOT NULL
+    role_name TEXT UNIQUE NOT NULL,
     role_hash TEXT UNIQUE NOT NULL
-};
+);
 
 -- app users
 CREATE TABLE users (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    fname TEXT,
-    lname TEXT,
+    first_name TEXT,
+    last_name TEXT,
     email TEXT,
     avatar BLOB
 );
 -- roles binding
 
 
-CREATE TABLE user_roles{
+CREATE TABLE user_roles(
     user_id INTEGER NOT NULL,
     role_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) 
-        REFERENCES users (user_id)
-    FOREIGN KEY 
-        REFERENCES roles (role_id)
+    FOREIGN KEY (user_id)REFERENCES users(user_id),
+    FOREIGN KEY (role_id) REFERENCES roles(role_id),
     PRIMARY KEY (user_id,role_id)
-};
+);
 
 CREATE TABLE extras_types (
     extras_type_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    type_text TEXT UNIQUE NOT NULL,
+    type_text TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE extras (
@@ -55,8 +54,7 @@ CREATE TABLE extras (
     name TEXT NOT NULL,
     type INTEGER,
     price REAL NOT NULL,
-    FOREIGN KEY (type)
-       REFERENCES extras_type (extras_type_id)
+    FOREIGN KEY (type) REFERENCES extras_type(extras_type_id)
 );
 
 CREATE TABLE bookings (
@@ -68,14 +66,13 @@ CREATE TABLE bookings (
         AS (unixepoch(date_to)-unixepoch(date_from)) VIRTUAL,
     price REAL NOT NULL,
     comment TEXT,
-    created_at INTEGER DEFAULT CURRENT_TIMESTAMP
-    FOREIGN KEY (user_id)
-       REFERENCES users (user_id),
+    created_at INTEGER DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE room_types (
     room_type_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    type TEXT UNIQUE NOT NULL,
+    type TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE rooms (
@@ -90,7 +87,6 @@ CREATE TABLE baskets (
     basket_id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     content BLOB,
-    FOREIGN KEY (user_id)
-        REFERENCES users (user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 )
 
